a4dc3e5db7e2d7ec6acce01b956ff8cf268b6708
Pankaj585 <96404243+Pankaj585@users.noreply.github.com>
2022-07-06 12:27:49 +0530

    Garage added

--- WheelsFury0.0/Assets/Scripts/MenuUIHandler.cs
+++ WheelsFury0.0/Assets/Scripts/MenuUIHandler.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class MenuUIHandler : MonoBehaviour
 {
-    [SerializeField] GameObject mainMenuUI;
-    [SerializeField] GameObject garageUI;
+    [SerializeField] GameObject mainMenuUI, garageUI, gameModeUI, settingsUI;
 
+    public void Play()
+    {
+        mainMenuUI.SetActive(false);
+        gameModeUI.SetActive(true);
+    }
     public void EnterGarage()
     {
         mainMenuUI.SetActive(false);
         garageUI.SetActive(true);
     }
-
+    public void Settings()
+    {
+        mainMenuUI.SetActive(false);
+        settingsUI.SetActive(true);
+    }
     public void enterMainMenu()
     {
         garageUI.SetActive(false);
+        gameModeUI.SetActive(false);
+        settingsUI.SetActive(false);
         mainMenuUI.SetActive(true);
     }
 }
